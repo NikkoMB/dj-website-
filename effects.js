486a19edc8a9bb4fd69eff64d4abaec57065a8a2
NikkoMB <bustillo.nikko@yahoo.com>
2019-12-13 12:29:48 -0500

    miami and 2nd cleveland now working

--- effects.js
+++ effects.js
@@ -3,9 +3,23 @@
 $(document).ready(function(){
     console.log('..LeBron on the fastbreak..')
 
-let audio = $("audio")[0];
+let audio = $("#c4u")[0];
 $("#cavs").mouseenter(function() {
   audio.play();
+  console.log("audio works for 2nd cavs");
 });
+
+
+let riley = $("#heat")[0];
+$("#miami").mouseenter(function() {
+  riley.play();
+  console.log("miami working");
 })
+
+
+
+
+
+
+});
 
