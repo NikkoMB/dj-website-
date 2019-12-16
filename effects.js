2d4588caa2e7299a96f25db08745c52b339e6709
NikkoMB <bustillo.nikko@yahoo.com>
2019-12-16 16:53:05 -0500

    video is now good

--- effects.js
+++ effects.js
@@ -9,6 +9,16 @@ $("#cavs").mouseenter(function() {
   console.log("audio works for 2nd cavs");
 });
 
+let noAudio = $("#c4u")[0];
+$("#cavs").mouseleave(function() {
+  audio.pause();
+  audio.currentTime = 0;
+  console.log("cavs working");
+})
+  console.log("audio works for 2nd cavs");
+
+
+//////////////////////////////////
 
 let riley = $("#heat")[0];
 $("#miami").mouseenter(function() {
@@ -16,11 +26,30 @@ $("#miami").mouseenter(function() {
   console.log("miami working");
 })
 
+let noRiley = $("#heat")[0];
+$("#miami").mouseleave(function() {
+  riley.pause();
+  riley.currentTime = 0;
+  console.log("miami working");
+}) 
+
+
+////////////////////////////////////
+
 let akron = $("#akron")[0];
 $("#cleveland").mouseenter(function() {
   akron.play();
   console.log("cavs working");
 })
+
+let akGone = $("#akron")[0];
+$("#cleveland").mouseleave(function() {
+  akron.pause();
+  akron.currentTime = 0;
+  console.log("cavs working");
+})
+
+//////////////////////////////
 
 let losAng = $("#laker")[0];
 $("#lakers").mouseenter(function() {
@@ -28,7 +57,11 @@ $("#lakers").mouseenter(function() {
   console.log("lakers working");
 })
 
-
+let losGone = $("#laker")[0];
+$("#lakers").mouseleave(function() {
+  losAng.pause();
+  losAng.currentTime = 0;
+  console.log("lakers working");
+})
 
 });
-
