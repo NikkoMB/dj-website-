3226a49e3a2d9655cccbfaa64bd6f86e3b813c4b
NikkoMB <bustillo.nikko@yahoo.com>
2020-01-16 10:57:19 -0500

    working on backend

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
 'use strict';
+const express = require('express');
+const path = require('path');
+
+const app = express(); 
+
+app.listen(process.env.PORT || 3000, () =>{
+    console.log('I Love You 3000');
+
+}); 
+
+app.use('/images', express.static(path.join(__dirname, '/images'))); 
+app.use('/main.css', express.static(path.join(__dirname, '/main.css'))); 
+
+//////////////////
+
+app.get('/index.html', (req,res) =>{
+    res.sendFile(path.join(__dirname,'index.html'));
+}); 
+
+app.get('/first_cavs.html', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/first_cavs.html'));
+});
+
+app.get('/lakers.html', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/lakers.html'));
+});
+
+app.get('/first_cavs.html', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/first_cavs.html'));
+});
+
+app.get('/first_cavs.html', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/first_cavs.html'));
+});
